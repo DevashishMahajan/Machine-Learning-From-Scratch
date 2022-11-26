2f7c420a3af33f53c672f200cdadbcf805ad0415
DevashishMahajan <devashishmahajan31@gmail.com>
2022-11-27 03:02:43 +0530

    code style warnings removed

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,67 +1,69 @@
 # Linear Regression From Scratch for Single feature using gradient descent
  
 # Importing necessary libraries
-import pandas as pd 
-import matplotlib.pyplot as plt 
-from sklearn.metrics import mean_squared_error,mean_absolute_error,r2_score
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 # %matplotlib inline
 
 # Read CSV file as dataframe
-Linear_data = pd.read_csv("LinearRegressionTest.csv") #LinearRegressionTest.csv   #Housing.csv
+Linear_data = pd.read_csv("LinearRegressionTest.csv")  #LinearRegressionTest.csv   #Housing.csv
 print(Linear_data)
 
 # Feature
-X_train = Linear_data['lotsize'].to_numpy().reshape(-1,1)
+X_train = Linear_data['lotsize'].to_numpy().reshape(-1, 1)
 
 # Label
-y_train= Linear_data['price']
+y_train = Linear_data['price']
 
 # Scaling features
 from sklearn.preprocessing import StandardScaler
 
-scaler=StandardScaler()
-X_train=scaler.fit_transform(X_train)
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
 
 print(X_train)
 print(y_train)
 
 # Scatter plot to visualize relation between feature and label
-plt.scatter(X_train,y_train)
+plt.scatter(X_train, y_train)
 plt.show()
 
+
 # Loss function Mean Square Error(MSE)
-def loss_function(m,b,X_train,y_train):
-    Square_Error=0
+def loss_function(m, b, X_train, y_train):
+    Square_Error = 0
     for i in range(len(y_train)):
-        x=X_train[i]
-        y=y_train[i]
+        x = X_train[i]
+        y = y_train[i]
         Square_Error += (y - (m * x + b))**2
     Mean_Square_Error = Square_Error/float(len(y_train))
     return Mean_Square_Error
 
-m=1
-b=0
-print("MSE =",loss_function(m,b,X_train,y_train))  # MSE = 1243019.4210526317
 
-m=2
-b=0
-print("MSE =",loss_function(m,b,X_train,y_train))  # MSE = 1215814.5263157894
+m = 1
+b = 0
+print("MSE =", loss_function(m, b, X_train, y_train))  # MSE = 1243019.4210526317
 
-m=5
-b=0
-print("MSE =",loss_function(m,b,X_train,y_train))  # MSE = 1136011.8421052631
+m = 2
+b = 0
+print("MSE =", loss_function(m, b, X_train, y_train))  # MSE = 1215814.5263157894
+
+m = 5
+b = 0
+print("MSE =", loss_function(m, b, X_train, y_train))  # MSE = 1136011.8421052631
 
 
 #### Gradient descent #### 
-def gradient_descent(X_train,y_train,m_now=1,b_now=0,alpha=0.001):
-    m_gradient=0 
-    b_gradient=0
+def gradient_descent(X_train, y_train, m_now=1, b_now=0, alpha=0.001):
+    m_gradient = 0
+    b_gradient = 0
 
     n=len(y_train)
 
     for i in range(n):
-        x=X_train[i]
-        y=y_train[i]
+        x = X_train[i]
+        y = y_train[i]
 
         m_gradient+= -(2)*x*(y- ((m_now *x) + b_now))
         b_gradient+= -(2) * (y - ((m_now *x) +b_now))
